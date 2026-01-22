325d219ce126b4b3af06a753e4d42867e8d2ea53
Anthoneloyd <sgarces0808@gmail.com>
2026-01-22 14:22:45 -0500

    corregido otro texto

--- lib/main.dart
+++ lib/main.dart
@@ -37,9 +37,7 @@ class MapPage extends StatelessWidget {
           ),
 
           RichAttributionWidget(
-            attributions: [
-              TextSourceAttribution('© OpenStreetMap contributors'),
-            ],
+            attributions: [TextSourceAttribution('OpenStreetMap contributors')],
           ),
         ],
       ),
